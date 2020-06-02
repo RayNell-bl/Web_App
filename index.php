e68182f61a99bdc782609a26723d15bb7723d527
RayNell-bl <62853698+RayNell-bl@users.noreply.github.com>
2020-06-03 02:30:47 +0600

    3

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$header = 'Главная: Ваше расписание занятий.';
+require_once 'template/header.php';
+?>
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box">
+            <section class="box-body">
+                <h3><?=$header;?></h3>
+            </section>
+        </div>
+    </div>
+</div>
+<?php
+require_once 'template/footer.php';
+?>
