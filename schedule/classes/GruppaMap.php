d1d755ae0d3d03f325e951ea14fb06c744dce581
RayNell-bl <62853698+RayNell-bl@users.noreply.github.com>
2020-06-07 17:07:45 +0600

    4

--- schedule/classes/GruppaMap.php
+++ schedule/classes/GruppaMap.php
@@ -3,6 +3,9 @@
 
 class GruppaMap extends BaseMap
 {
-
+	public function arrGruppas(){
+        $res = $this->db->query("SELECT group_id AS id, name AS value FROM groups");
+        return $res->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
 ?>
